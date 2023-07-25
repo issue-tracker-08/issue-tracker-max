992388f88dc9de8728b4b5788b66146307ad8b3c
유지수 Jisoo Yoo <youzysu@gmail.com>
2023-07-25 10:32:10 +0900

    #7 design: 디자인 시스템 세팅 (#8)
    * #7 design: 디자인 시스템 세팅
    
    - 로고 컴포넌트, GlobalStyle, designSystem 생성
    - icon svg 파일 업로드
    - Pretendard font
    
    * #7 refactor: 로고 컴포넌트 props 구조 수정
    
    * #7 refactor: 디자인 시스템 세팅 리뷰 반영
    
    - design modules 불필요한 변수 선언 없이 export default
    - src 최상단 디렉터리 path alias 선언

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,7 +1,19 @@
-import "./App.css";
+import Logo from "@components/common/Logo";
+import GlobalStyle from "@styles/GlobalStyle";
+import designSystem from "@styles/designSystem";
+import styled, { ThemeProvider } from "styled-components";
 
-function App() {
-  return <div></div>;
+export default function App() {
+  return (
+    <ThemeProvider theme={designSystem}>
+      <GlobalStyle />
+      <Logo size="large" />
+      <H1>적용 되니?</H1>
+    </ThemeProvider>
+  );
 }
 
-export default App;
+const H1 = styled.h1`
+  color: ${({ theme: { colors } }) => colors.navy};
+  font: ${({ theme: { font } }) => font.displayMD12};
+`;
