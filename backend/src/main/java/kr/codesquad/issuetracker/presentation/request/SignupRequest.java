8eeb4849f4be195975a6e746dc0ae47d18163311
pie <yejinh_h@naver.com>
2023-07-25 23:02:21 +0900

    test : 회원가입 Controller 테스트 작성

--- backend/src/main/java/kr/codesquad/issuetracker/presentation/request/SignupRequest.java
+++ backend/src/main/java/kr/codesquad/issuetracker/presentation/request/SignupRequest.java
@@ -2,9 +2,11 @@ package kr.codesquad.issuetracker.presentation.request;
 
 import javax.validation.constraints.Size;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 public class SignupRequest {
@@ -14,3 +16,5 @@ public class SignupRequest {
 	@Size(min = 6, max = 12, message = "비밀번호는 6자리 이상, 12자리 이하여야 합니다.")
 	private String password;
 }
+
+
