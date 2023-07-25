3e0fff71dc3350a03c9fb4f0476365d01ad46e8f
Jeongyong Park <1239824@naver.com>
2023-07-25 16:41:23 +0900

    feat: 예외처리 기능 개발

---
+++ backend/src/main/java/kr/codesquad/issuetracker/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package kr.codesquad.issuetracker.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<String> handleValidException(MethodArgumentNotValidException e) {
+		String exMessage = e.getBindingResult().getFieldError().getDefaultMessage();
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exMessage);
+	}
+
+	@ExceptionHandler(ApplicationException.class)
+	public ResponseEntity<String> handleApplicationException(ApplicationException e) {
+		return ResponseEntity.status(e.getErrorCode().getStatusCode()).body(e.getErrorCode().getMessage());
+	}
+}
