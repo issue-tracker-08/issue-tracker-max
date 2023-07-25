cbcae16f23a8f0fd7f303c6292b66ffcf29f57c5
Jeongyong Park <1239824@naver.com>
2023-07-25 16:41:01 +0900

    feat: 회원가입 기능 개발
    - 이미 존재하는 로그인 아이디를 가진 회원이 있으면 예외던짐
    - 비밀번호 암호화 저장
    - 회원가입시 유효성 검증

---
+++ backend/src/main/java/kr/codesquad/issuetracker/presentation/request/SignupRequest.java
@@ -0,0 +1,16 @@
+package kr.codesquad.issuetracker.presentation.request;
+
+import javax.validation.constraints.Size;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class SignupRequest {
+
+	@Size(min = 6, max = 16, message = "아이디는 6자리 이상, 16자리 이하여야 합니다.")
+	private String loginId;
+	@Size(min = 6, max = 12, message = "비밀번호는 6자리 이상, 12자리 이하여야 합니다.")
+	private String password;
+}
