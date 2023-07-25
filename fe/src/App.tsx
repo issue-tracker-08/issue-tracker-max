d99c80791dcbcf2d80d927369697e1d296aafe17
Daeram Chung <daeram.chung@gmail.com>
2023-07-25 11:12:01 +0900

    #7 design: light/dark 모드 design system 세팅

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,19 +1,29 @@
+import { useState } from "react";
 import Logo from "@components/common/Logo";
 import GlobalStyle from "@styles/GlobalStyle";
-import designSystem from "@styles/designSystem";
+import { lightMode, darkMode } from "@styles/designSystem";
 import styled, { ThemeProvider } from "styled-components";
 
 export default function App() {
+  const [themeMode, setThemeMode] = useState<"light" | "dark">("dark");
+
+  const toggleThemeMode = () => {
+    setThemeMode((prev) => {
+      return prev === "light" ? "dark" : "light";
+    });
+  };
+
   return (
-    <ThemeProvider theme={designSystem}>
+    <ThemeProvider theme={themeMode === "light" ? lightMode : darkMode}>
       <GlobalStyle />
       <Logo size="large" />
       <H1>적용 되니?</H1>
+      <button onClick={toggleThemeMode}>Theme Mode</button>
     </ThemeProvider>
   );
 }
 
 const H1 = styled.h1`
-  color: ${({ theme: { colors } }) => colors.navy};
+  color: ${({ theme: { neutral } }) => neutral.text.strong};
   font: ${({ theme: { font } }) => font.displayMD12};
 `;
