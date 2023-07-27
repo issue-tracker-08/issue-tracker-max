13615244d6beaed48e605591e45935f0e82be1a0
Jeongyong Park <1239824@naver.com>
2023-07-27 14:52:07 +0900

    feat: Jwt를 검증하는 필터 추가

--- backend/src/main/java/kr/codesquad/issuetracker/infrastructure/config/SecurityConfig.java
+++ backend/src/main/java/kr/codesquad/issuetracker/infrastructure/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package kr.codesquad.issuetracker.infrastructure.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -7,13 +8,27 @@ import org.springframework.context.annotation.Import;
 import kr.codesquad.config.JwtConfig;
 import kr.codesquad.issuetracker.infrastructure.security.hash.PasswordEncoder;
 import kr.codesquad.issuetracker.infrastructure.security.hash.SHA256;
+import kr.codesquad.issuetracker.infrastructure.security.jwt.JwtProvider;
+import kr.codesquad.issuetracker.presentation.filter.JwtFilter;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 @Import(JwtConfig.class)
 @Configuration
 public class SecurityConfig {
 
+	private final JwtProvider jwtProvider;
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new SHA256();
+	}
+
+	@Bean
+	public FilterRegistrationBean<JwtFilter> jwtFilter() {
+		FilterRegistrationBean<JwtFilter> jwtFilter = new FilterRegistrationBean<>();
+		jwtFilter.setFilter(new JwtFilter(jwtProvider));
+		jwtFilter.addUrlPatterns("/api/*");
+		return jwtFilter;
 	}
 }
