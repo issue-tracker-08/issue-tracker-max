47a0be49d0f9f296fb13462535a63f5d68d280d7
Daeram Chung <daeram.chung@gmail.com>
2023-07-28 10:56:26 +0900

    #31 design: IssuesPage, TableBodyIssues, IssueItem 초기 구현

--- fe/src/components/Table/index.ts
+++ fe/src/components/Table/index.ts
@@ -1,4 +1,5 @@
 import Table from "./Table";
+import TableBodyIssues from "./TableBodyIssues";
 import TableHeaderIssues from "./TableHeaderIssues";
 
-export { Table, TableHeaderIssues };
+export { Table, TableBodyIssues, TableHeaderIssues };
