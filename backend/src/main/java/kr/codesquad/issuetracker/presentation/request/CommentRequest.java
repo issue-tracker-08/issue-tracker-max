154dd1f8f3566001324bbc271e8304d32580ccb7
pie <yejinh_h@naver.com>
2023-08-01 15:18:27 +0900

    feat: 댓글 글자 수 제한

--- backend/src/main/java/kr/codesquad/issuetracker/presentation/request/CommentRequest.java
+++ backend/src/main/java/kr/codesquad/issuetracker/presentation/request/CommentRequest.java
@@ -1,6 +1,7 @@
 package kr.codesquad.issuetracker.presentation.request;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
 @Getter
 public class CommentRequest {
 
-	@NotEmpty(message = "댓글의 내용은 비워둘 수 없습니다.")
+	@NotEmpty(message = "댓글의 본문은 비워둘 수 없습니다.")
+	@Size(max = 256, message = "댓글의 본문은 256자를 넘길 수 없습니다.")
 	private String content;
 }
