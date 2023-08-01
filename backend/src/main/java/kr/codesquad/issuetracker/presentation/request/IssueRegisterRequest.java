f816032d43e0afbb4d9a6a8815f62acd0f980a0a
Jeongyong Park <1239824@naver.com>
2023-08-01 16:45:12 +0900

    feat: DB 컬럼 제약조건에 맞게 글자수 제한 추가

--- backend/src/main/java/kr/codesquad/issuetracker/presentation/request/IssueRegisterRequest.java
+++ backend/src/main/java/kr/codesquad/issuetracker/presentation/request/IssueRegisterRequest.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,6 +15,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class IssueRegisterRequest {
 
+	@Size(max = 45, message = "이슈의 제목은 45자를 넘을 수 없습니다.")
 	@NotEmpty(message = "이슈의 제목은 비워둘 수 없습니다.")
 	private String title;
 	private String content;
