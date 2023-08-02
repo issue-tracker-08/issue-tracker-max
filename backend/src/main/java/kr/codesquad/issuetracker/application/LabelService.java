bc45fc1a251d66e1eff6dd321739bd7b86693f24
code-tree <dltjdqls1200@naver.com>
2023-08-02 13:33:53 +0900

    feat: label 목록 조회 기능 추가

---
+++ backend/src/main/java/kr/codesquad/issuetracker/application/LabelService.java
@@ -0,0 +1,29 @@
+package kr.codesquad.issuetracker.application;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import kr.codesquad.issuetracker.infrastructure.persistence.LabelRepository;
+import kr.codesquad.issuetracker.presentation.response.LabelResponse;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class LabelService {
+
+	private final LabelRepository labelRepository;
+
+	@Transactional(readOnly = true)
+	public List<LabelResponse> findAll() {
+		return labelRepository.findAll().stream()
+			.map(label -> new LabelResponse(
+				label.getId(), label.getName(), label.getFontColor(), label.getBackgroundColor()
+			))
+			.sorted(Comparator.comparing(LabelResponse::getName))
+			.collect(Collectors.toList());
+	}
+}
