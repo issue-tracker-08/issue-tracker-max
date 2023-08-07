90af68f15fff72476b81e89448b47ad3833e23bb
Jeongyong Park <1239824@naver.com>
2023-08-07 16:18:58 +0900

    refactor: 불필요한 쿼리를 날리지 않도록 수정

--- backend/src/main/java/kr/codesquad/issuetracker/application/CommentService.java
+++ backend/src/main/java/kr/codesquad/issuetracker/application/CommentService.java
@@ -29,7 +29,7 @@ public class CommentService {
 	public void modify(String modifiedComment, Integer commentId, Integer userId) {
 		Comment comment = commentRepository.findById(commentId)
 			.orElseThrow(() -> new ApplicationException(ErrorCode.COMMENT_NOT_FOUND));
-		if (comment.getUserAccountId().equals(userId)) {
+		if (!comment.getUserAccountId().equals(userId)) {
 			throw new ApplicationException(ErrorCode.NO_AUTHORIZATION);
 		}
 		comment.modifyContent(modifiedComment);
