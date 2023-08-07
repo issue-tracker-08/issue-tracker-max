c1fa1d21bd5c94b40c6a90dacb10637c5a58bea3
Daeram Chung <daeram.chung@gmail.com>
2023-08-07 17:40:17 +0900

    #94 refactor: 이슈 디테일 상태 관리 접근 조정
    - `IssueDetailPage`단에서 상세 detail 정보 fetch.
      - `/details`, `/sidebar`, `/comments`

--- fe/src/pages/MainPage/IssueDetailPage.tsx
+++ fe/src/pages/MainPage/IssueDetailPage.tsx
@@ -1,15 +1,46 @@
-import IssueDetailBody from "@components/Issues/IssueDetail/IssueDetailBody";
 import IssueDetailHeader from "@components/Issues/IssueDetail/IssueDetailHeader";
+import { IssueDetails } from "@customTypes/index";
+import useFetch from "@hooks/useFetch";
+import { getIssue } from "api";
+import { useCallback } from "react";
 import { useParams } from "react-router-dom";
 
 export default function IssueDetailPage() {
   const { issueId } = useParams();
   const issueNumber = parseInt(issueId!);
 
+  const {
+    data: issueDetails,
+    errorMessage: issueDetailsErrorMessage,
+    setData: updateIssueDetails,
+  } = useFetch<IssueDetails>(
+    useCallback(() => getIssue(issueNumber), [issueNumber])
+  );
+
+  // TODO: sidebar useFetch
+  // TODO: comments useFetch
+
+  const updateIssueTitle = (newTitle: string) => {
+    updateIssueDetails((prev) => {
+      return prev ? { ...prev, title: newTitle } : prev;
+    });
+  };
+
+  // TODO: fetch comments
+  const comments = [];
+
   return (
     <>
-      <IssueDetailHeader issueNumber={issueNumber} />
-      <IssueDetailBody issueNumber={issueNumber} />
+      {issueDetails && (
+        <IssueDetailHeader
+          {...{
+            issueDetails,
+            issueDetailsErrorMessage,
+            updateIssueTitle,
+            numComments: comments.length,
+          }}
+        />
+      )}
     </>
   );
 }
