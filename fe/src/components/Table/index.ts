f8b0846a963b2cb18ce05e9449a417d6cc2b6a7a
Daeram Chung <daeram.chung@gmail.com>
2023-08-09 03:40:18 +0900

    #104 design: TableHeaderLabel 컴포넌트 구현
    - `TableHeader` 템플릿 컴포넌트를 사용하여 `TableHeaderLabel` 컴포넌트 구현.

--- fe/src/components/Table/index.ts
+++ fe/src/components/Table/index.ts
@@ -1,5 +1,6 @@
 import Table from "./Table";
 import TableBodyIssues from "./TableBodyIssues";
 import TableHeaderIssues from "./TableHeaderIssues";
+import TableHeaderLabels from "./TableHeaderLabels";
 
-export { Table, TableBodyIssues, TableHeaderIssues };
+export { Table, TableBodyIssues, TableHeaderIssues, TableHeaderLabels };
