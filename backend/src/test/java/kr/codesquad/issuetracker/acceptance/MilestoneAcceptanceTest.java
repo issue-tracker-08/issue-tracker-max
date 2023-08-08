7ce8dee92ee2ffb7074632cc0c3a52f5c1ad6c1b
Jeongyong Park <1239824@naver.com>
2023-08-08 14:29:06 +0900

    test: 마일스톤 등록 테스트코드 작성

---
+++ backend/src/test/java/kr/codesquad/issuetracker/acceptance/MilestoneAcceptanceTest.java
@@ -0,0 +1,39 @@
+package kr.codesquad.issuetracker.acceptance;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+
+import io.restassured.RestAssured;
+
+public class MilestoneAcceptanceTest extends AcceptanceTest {
+
+	@DisplayName("마일스톤 등록에 성공한다.")
+	@Test
+	void register() {
+		// given
+		var given = RestAssured
+			.given().log().all()
+			.header(HttpHeaders.AUTHORIZATION, "Bearer " + jwtProvider.createToken("1").getAccessToken())
+			.contentType(MediaType.APPLICATION_JSON_VALUE)
+			.body(Map.of(
+				"milestoneName", "BE 1주차 스프린트",
+				"description", "화이팅!",
+				"dueDate", "2023-09-01 00:00:00"));
+
+		// when
+		var response = given
+			.when()
+			.post("/api/milestones")
+			.then().log().all()
+			.extract();
+
+		// then
+		assertThat(response.statusCode()).isEqualTo(201);
+	}
+}
