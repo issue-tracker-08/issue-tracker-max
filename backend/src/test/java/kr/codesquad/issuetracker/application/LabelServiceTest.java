e5d0e0f319cdaa6d1a6c3c8dddd9d89ed93cdcea
Jeongyong Park <1239824@naver.com>
2023-08-09 15:35:24 +0900

    feat: 라벨 전체 목록 조회 response API 수정

--- backend/src/test/java/kr/codesquad/issuetracker/application/LabelServiceTest.java
+++ backend/src/test/java/kr/codesquad/issuetracker/application/LabelServiceTest.java
@@ -4,21 +4,16 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.util.Comparator;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import kr.codesquad.issuetracker.ApplicationTest;
-import kr.codesquad.issuetracker.acceptance.DatabaseInitializer;
 import kr.codesquad.issuetracker.presentation.response.LabelResponse;
 
 @ApplicationTest
 class LabelServiceTest {
-
-	@Autowired
-	private DatabaseInitializer databaseInitializer;
 
 	@Autowired
 	private LabelService labelService;
@@ -26,15 +21,16 @@ class LabelServiceTest {
 	@MockBean
 	private S3Service s3Service;
 
-	@BeforeEach
-	void setUp() {
-		databaseInitializer.initTables();
-	}
-
 	@DisplayName("label 목록 조회시 이름순으로 정렬이 된다.")
 	@Test
 	void findLabelsSortedNameAscending() {
+		// given
+		labelService.register("feat", "기능 개발", "#FFF", "#EDE");
+		labelService.register("setting", "기능 개발", "#FFF", "#EDE");
+		labelService.register("backend", "기능 개발", "#FFF", "#EDE");
+
+		// when & then
 		assertThat(labelService.findAll())
-			.isSortedAccordingTo(Comparator.comparing(LabelResponse::getLabelName));
+			.isSortedAccordingTo(Comparator.comparing(LabelResponse::getName));
 	}
 }
