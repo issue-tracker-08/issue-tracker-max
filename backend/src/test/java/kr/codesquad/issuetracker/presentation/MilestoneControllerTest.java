75007c4ed66c44f6351d8bc31cc6cf8e495d8e32
Jeongyong Park <1239824@naver.com>
2023-08-09 12:03:34 +0900

    test: 마일스톤 수정 테스트코드 작성

--- backend/src/test/java/kr/codesquad/issuetracker/presentation/MilestoneControllerTest.java
+++ backend/src/test/java/kr/codesquad/issuetracker/presentation/MilestoneControllerTest.java
@@ -39,7 +39,8 @@ class MilestoneControllerTest extends ControllerTest {
 					post("/api/milestones")
 						.contentType(MediaType.APPLICATION_JSON)
 						.header(HttpHeaders.AUTHORIZATION, "Bearer " + jwtProvider.createToken("1").getAccessToken())
-						.content(objectMapper.writeValueAsBytes(FixtureFactory.createMilestoneRegisterRequest("1주차 마일스톤"))))
+						.content(objectMapper.writeValueAsBytes(
+							FixtureFactory.createMilestoneCommonRequest("1주차 마일스톤"))))
 				.andExpect(status().isCreated())
 				.andDo(print());
 		}
@@ -55,7 +56,47 @@ class MilestoneControllerTest extends ControllerTest {
 					post("/api/milestones")
 						.contentType(MediaType.APPLICATION_JSON)
 						.header(HttpHeaders.AUTHORIZATION, "Bearer " + jwtProvider.createToken("1").getAccessToken())
-						.content(objectMapper.writeValueAsString(FixtureFactory.createMilestoneRegisterRequest(""))))
+						.content(objectMapper.writeValueAsString(FixtureFactory.createMilestoneCommonRequest(""))))
+				.andExpect(status().isBadRequest())
+				.andDo(print());
+		}
+	}
+
+	@DisplayName("마일스톤을 수정할 때 ")
+	@Nested
+	class MilestoneModifyTest {
+
+		@DisplayName("마일스톤 수정에 성공한다.")
+		@Test
+		void modify() throws Exception {
+			// given
+			willDoNothing().given(milestoneService)
+				.modify(anyInt(), anyString(), anyString(), any(LocalDateTime.class));
+
+			// when & then
+			mockMvc.perform(
+					put("/api/milestones/1")
+						.contentType(MediaType.APPLICATION_JSON)
+						.header(HttpHeaders.AUTHORIZATION, "Bearer " + jwtProvider.createToken("1").getAccessToken())
+						.content(objectMapper.writeValueAsBytes(
+							FixtureFactory.createMilestoneCommonRequest("1주차 마일스톤"))))
+				.andExpect(status().isOk())
+				.andDo(print());
+		}
+
+		@DisplayName("빈 마일스톤 이름이 들어오면 400응답을 한다.")
+		@Test
+		void givenInvalidRegisterInfo_thenResponse400() throws Exception {
+			// given
+			willDoNothing().given(milestoneService)
+				.modify(anyInt(), anyString(), anyString(), any(LocalDateTime.class));
+
+			// when & then
+			mockMvc.perform(
+					put("/api/milestones/1")
+						.contentType(MediaType.APPLICATION_JSON)
+						.header(HttpHeaders.AUTHORIZATION, "Bearer " + jwtProvider.createToken("1").getAccessToken())
+						.content(objectMapper.writeValueAsString(FixtureFactory.createMilestoneCommonRequest(""))))
 				.andExpect(status().isBadRequest())
 				.andDo(print());
 		}
