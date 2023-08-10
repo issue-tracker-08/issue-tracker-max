33fe55d216468ac4dbe229f35b8ceb892dc2da4e
Daeram Chung <daeram.chung@gmail.com>
2023-08-10 15:08:42 +0900

    #104 rename: decToHex -> decToHexOpacity

--- fe/src/styles/designSystem.ts
+++ fe/src/styles/designSystem.ts
@@ -1,4 +1,4 @@
-import { decToHex } from "@utils/style";
+import { decToHexOpacity } from "@utils/style";
 
 const colors = {
   grey50: "#FEFEFE",
@@ -96,7 +96,7 @@ export const lightMode = {
     navy: colors.navy,
     red: colors.red,
   },
-  boxShadow: `0px 0px 8px 0px ${colors.grey900}${decToHex(
+  boxShadow: `0px 0px 8px 0px ${colors.grey900}${decToHexOpacity(
     opacity.transparent
   )}`,
   colors,
@@ -161,7 +161,9 @@ export const darkMode = {
     navy: colors.navy,
     red: colors.red,
   },
-  boxShadow: `0px 0px 16px 0px ${colors.grey900}${decToHex(opacity.hover)}`,
+  boxShadow: `0px 0px 16px 0px ${colors.grey900}${decToHexOpacity(
+    opacity.hover
+  )}`,
   colors,
   border,
   radius,
